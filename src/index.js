8499577999a937d05628fe20ccebfe77901bdd8c
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-06-17 04:48:35 -0500

    [feat] create views and styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const Handlebars = require('handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const hbs = require('express-handlebars')
+const path = require('path')
+const router = require('./routes')
 const { port, dev } = require('./config')
 
 
-// Init
+// Settings
 const app = express()
 require('./database')
+
+app.engine('hbs', hbs({
+	handlebars: allowInsecurePrototypeAccess(Handlebars),
+	defaultLayout: 'index',
+	extname: '.hbs'
+}))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 
 // Middlewares
@@ -13,8 +26,13 @@ if (dev) {
 	app.use(morgan('dev'))
 }
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-// Server
+// Routes
+router(app)
+
+
+// Init
 app.listen(port, dev ? console.log(`I'm alive on: http://localhost:${port}`) : undefined)
